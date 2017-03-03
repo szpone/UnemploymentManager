c13206b9d5f752b074bbbf1b97524c8896cfcc02
Nikola Adamus <nikola.adamus@gmail.com>
2017-03-03 14:49:36 +0100

    update company model with sent date and commnet

--- unemployment/manager/models.py
+++ unemployment/manager/models.py
@@ -3,8 +3,16 @@ from django.db import models
 
 # Create your models here.
 
-class CompanyName(models.Model):
+# a model describing company what we applied to
+class Company(models.Model):
     name = models.CharField(max_length=128)
     email = models.CharField(max_length=64)
+    company_website = models.CharField(max_length=128)
+    offer_website = models.CharField(max_length=128)
     position = models.ForeignKey(Position, on_delete=models.CASCADE)
     category = models.OneToOneField(Category, on_delete=models.CASCADE, primary_key=True)
+    sent_date = models.DateTimeField(auto_now=True)
+    comment = models.TextField(blank=True)
+
+
+
