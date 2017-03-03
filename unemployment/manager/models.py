a3c501ea5e56b9d2a91514098f0d366ea4288fa8
Nikola Adamus <nikola.adamus@gmail.com>
2017-03-03 14:38:27 +0100

    create model for company

--- unemployment/manager/models.py
+++ unemployment/manager/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+
+class CompanyName(models.Model):
+    name = models.CharField(max_length=128)
+    email = models.CharField(max_length=64)
+    position = models.ForeignKey(Position, on_delete=models.CASCADE)
+    category = models.OneToOneField(Category, on_delete=models.CASCADE, primary_key=True)
